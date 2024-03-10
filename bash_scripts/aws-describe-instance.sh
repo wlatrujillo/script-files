0525e0806617fa2c4ba6a834e1bad4cd04fa860d
Nelson Trujillo <nelson.trujillo@cobistopaz.com>
2024-03-10 12:18:02 -0500

    refactor: aws describe instances

--- bash_scripts/aws-describe-instance.sh
+++ bash_scripts/aws-describe-instance.sh
@@ -1,17 +1,72 @@
 #!/bin/sh
-# This script will get the instance id, instance type, public ip, private ip, and availability zone of the instance
-# and print it to the console
+# This script gets the instance id, instance type, public ip, private ip, and availability zone of the instance
+# and prints it to the console
 
-# Usage: ./aws_instance_info.sh <instance_name> <profile> <property>
-name=$1
-property=$2
-profile=wladi.trujillo
+DisplayHelp()
+{
+   # Display Help
+   echo
+   echo "Help:"
+   echo
+   echo "Get the instance id, instance type, public ip, private ip, and availability zone of the instance and print it to the console"
+   echo
+   echo "Syntax: scriptTemplate [-p profile] [-h] <instance_name> <property>"
+   echo
+   echo "Options:"
+   echo "  -p    Specify the AWS profile."
+   echo "  -h    Print this Help."
+   echo
+   echo "Arguments:"
+   echo "  Usage example: $0 mongodb PublicIpAddress"
+   echo "  Usage example: $0 mongodb PrivateIpAddress"
+   echo "  Usage example: $0 mongodb InstanceId"
+   echo "  Usage example: $0 mongodb InstanceType"
+}
 
-if [ ! "$name" ]  || [ ! "$property" ];
-then
-  echo "Usage: $0 <instance_name>"
-  echo "PublicIpAddress, PrivateIpAddress, InstanceId, InstanceType, AvailabilityZone"
-  exit
+CallAwsCli()
+{
+    if [ ! "$1" ] || [ ! "$2" ];
+    then
+        echo "Error: Invalid arguments profile: $1 name: $2"
+        DisplayHelp
+        exit 1
+    fi
+
+    if [ ! "$3" ];
+    then
+        aws ec2 --profile "$1" describe-instances --filters "Name=tag:Name,Values=$2"
+        exit 0
+    fi
+
+    aws ec2 --profile "$1" describe-instances --filters "Name=tag:Name,Values=$2" | grep "$3" | awk '{ print $2 }' | tr -d '",' | head -n 1
+}
+
+# Set default profile
+profile="wladi.trujillo"
+
+# Get the options
+while getopts ":hp:" option; do
+   case $option in
+      h) # Display Help
+         DisplayHelp
+         exit;;
+      p) # Specify AWS profile
+         profile="$OPTARG"
+         ;;
+      \?) # Incorrect option
+         echo "Error: Invalid option"
+         exit;;
+   esac
+done
+
+shift $((OPTIND - 1))
+
+# Check if enough arguments are provided
+if [ "$#" -ne 2 ]; then
+    echo "Error: Invalid number of arguments"
+    DisplayHelp
+    exit 1
 fi
 
-aws ec2 --profile $profile describe-instances --filters "Name=tag:Name,Values=$name" | grep $property | awk '{ print $2 }' | tr -d '",' | head -n 1
+# Call AWS CLI function
+CallAwsCli "$profile" "$1" "$2"
