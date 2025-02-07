645daa52efa95d2137675abf1f2c7670e75df692
Nelson Trujillo <nelson.trujillo@cobistopaz.com>
2025-02-07 10:56:31 -0500

    fix: script cwc

---
+++ powershell_scripts/cobis-cwc.bat
@@ -0,0 +1,72 @@
+
+@echo off
+setlocal
+rem This script is to download cwc image locally.
+rem This script takes 3 arguments, customer environment containerTagId.
+
+set customer=%1
+set env=%2
+set containerTag=%3
+
+if not defined customer (
+    echo customer env containerTag are needed. Example: cobis-cwc.bat cobis dev1 1.2.45-SNAPSHOT
+    exit /b 1
+)
+if not defined env (
+    echo customer env containerTag are needed. Example: cobis-cwc.bat cobis dev1 1.2.45-SNAPSHOT
+    exit /b 1
+)
+if not defined containerTag (
+    echo customer env containerTag are needed. Example: cobis-cwc.bat cobis dev1 1.2.45-SNAPSHOT
+    exit /b 1
+)
+
+if "%customer%"=="cobis" goto cobis
+
+:cobis
+if "%env%"=="dev1" (
+    set account=681989517074
+    set profile=681989517074_COBDeveloper
+    set region=us-east-1
+)
+if "%env%"=="qa1" (
+    set account=110595436954
+    set profile=110595436954_COBSupport
+    set region=us-east-1
+)
+
+echo "Login aws ecr profile: %profile%"
+aws ecr --profile %profile% get-login-password --region %region% | docker login --username AWS --password-stdin %account%.dkr.ecr.%region%.amazonaws.com
+
+echo "pull containerTag: $containerTag"
+docker pull %account%.dkr.ecr.%region%.amazonaws.com/%env%/cwc-cloud:%containerTag%
+
+echo "run container: $containerTag"
+docker run --name %containerTag% -d %account%.dkr.ecr.%region%.amazonaws.com/%env%/cwc-cloud:%containerTag%
+
+set targetPath = Get-Location
+
+echo "Copy cobishome-web to: ${targetPath}"
+docker cp %containerTag%:/home/cobisuser/cobishome-web ${targetPath}
+
+echo "Copy tomcat to: ${targetPath}"
+docker cp %containerTag%:/usr/local/tomcat ${targetPath}
+
+echo "stopping docker: %containerTag%"
+docker stop %containerTag%
+
+echo "removing docker: $containerTag"
+docker rm %containerTag%
+
+set infrastructurePath = "%targetPath%\\cobishome-web\\cwc\\infrastructure"
+set cobisContainerPath = "%targetPath%\\cobishome-web\\cwc\\services-as\\cobis-container"
+set tomcatBinPath = "%targetPath%\\tomcat\\bin"
+
+echo "Copy cwc-log-config.xml to: %infrastructurePath%"
+Copy-Item "%targetPath%\\cwc-assets\\cwc-log-config.xml" %infrastructurePath%
+
+echo "Copy cobis-container-config.xml settings to: %cobisContainerPath%"
+Copy-Item "%targetPath%\\cwc-assets\\cobis-container-config.xml" $cobisContainerPath 
+
+echo "Copy setenv.bat to: %tomcatBinPath%"
+Copy-Item "%targetPath%\\cwc-assets\\cobis\\setenv-%env%.bat" "%tomcatBinPath%\\setenv.bat"
